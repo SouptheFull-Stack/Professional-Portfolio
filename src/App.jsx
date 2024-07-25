c16539c8102a900d61177dd7a3ba798b147024b7
Mitra USYD <mitraahmadi129@gmail.com>
2024-07-26 02:52:39 +1000

    editing App to add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-// import Nav from './components/NavTabs';
+import Nav from './components/NavTabs';
 
 export default function App() {
     return (
         <>
-            <NavTabs /> 
-            <main className="bodyBack">
+            <Nav /> 
+            <main>
                 <Outlet />
             </main>
         </>
