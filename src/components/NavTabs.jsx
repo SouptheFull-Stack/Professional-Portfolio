29f4c1503d7ed616e9661d6833329f6ec0ee0df9
Mitra USYD <mitraahmadi129@gmail.com>
2024-07-26 02:53:56 +1000

    importing css for testing and experimenting for pages

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,45 +1,48 @@
 import { Link, useLocation } from 'react-router-dom';
+import '../App.css'
 
 export default function NavTabs() {
     const currentPage = useLocation().pathname;
 
     return (
-        <ul>
-            <li>
+        <nav className='nav'>
+        <ul className="mitraNav">
+            <li className="nav-button">
                 <Link to="/"
-                className={currentPage === "/" ? 'nav-link active' : 'nav-link'}
+                className={currentPage === "/" ? 'nav-link active buttons' : 'nav-link buttons'}
                 >
                     Home
                 </Link>
             </li>
-            <li>
+            <li className="nav-button"> 
                 <Link to="/About"
-                className={currentPage === "/About"}
+                className={currentPage === "/About" ? 'nav-link active buttons' : 'nav-link buttons'}
                 >
                     About
                 </Link>
             </li>
-            <li>
+            <li className="nav-button">
                 <Link to="/Portfolio"
-                className={currentPage === '/Portfolio'}
+                className={currentPage === '/Portfolio' ? 'nav-link active buttons' : 'nav-link buttons'}
                 >
                     Portfolio
                 </Link>
             </li>
-            <li>
+            <li className="nav-button">
                 <Link to="/Resume"
-                className={currentPage === "/Resume"}
+                className={currentPage === "/Resume" ? 'nav-link active buttons' : 'nav-link buttons'}
                 >
                     Resume
                 </Link>
             </li>
-            <li>
+            <li className="nav-button">
                 <Link to="/Contact"
-                className={currentPage === "/Contact"}
+                className={currentPage === "/Contact" ? 'nav-link active buttons' : 'nav-link buttons'}
                 >
                     Contact
                 </Link>
             </li>
         </ul>
+        </nav>
     );
 }
