46cd5957841ee0db6f57004ca9fb27d83f1cb05e
Mitra USYD <mitraahmadi129@gmail.com>
2024-07-26 02:54:31 +1000

    writing up npm package for fancy typewriter effect, need to work on further and test/experiment

---
+++ src/components/Typewriter.jsx
@@ -0,0 +1,16 @@
+import { useTypewriter } from 'react-simple-typewriter';
+
+const Typewriter = ({ words, loop, typeSpeed, cursor, cursorStyle, cursorBlinking }) => {
+  const [typeEffect] = useTypewriter({
+    words: words,
+    loop: loop,
+    typeSpeed: typeSpeed,
+    cursor: cursor,
+    cursorStyle: `{${cursorStyle}}`,
+    cursorBlinking: cursorBlinking
+  });
+
+  return <h1 className='customFont'>{typeEffect}</h1>;
+};
+
+export default Typewriter;
