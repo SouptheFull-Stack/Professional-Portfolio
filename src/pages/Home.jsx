52d3ebcdc076a3a645bc31832e5e49337e171feb
Mitra USYD <mitraahmadi129@gmail.com>
2024-07-26 02:56:09 +1000

    setting up props of typewriter component for testing and such

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react';
 import Typewriter from '../components/Typewriter';
+// import Typewriter from '../components/Typewriter';
 
 export default function Home() {
+
     return (
-    <div>
-        <Typewriter />
+    <div className="has-text-centered">
+        <Typewriter 
+          words={['Hello, my name is Mitra.', 'I am a passionate MERN stack developer.']}
+          loop={1}
+          typeSpeed={100}
+          cursor={true}
+          cursorStyle= "'|'"
+          cursorBlinking={true}
+        />
     </div>
     )
-}
-
-
+} 
