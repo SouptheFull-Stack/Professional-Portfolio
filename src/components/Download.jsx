421943a9dfe7a41ecae188cf6c723680c277c573
Mitra USYD <mitraahmadi129@gmail.com>
2024-07-26 02:53:15 +1000

    adding in component for downloading my CV

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function DownloadPDF() {
     return (
         <div>
-            <a>Download CV</a>
+            <a href="../../Mitra_CV.pdf" download="Mitra_CV.pdf">
+                <button>Download CV</button>
+            </a>
         </div>
     )
 }
