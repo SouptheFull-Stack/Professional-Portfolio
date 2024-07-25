2ba46429c7e7bab58648320ebab0824c8f481f62
Mitra USYD <mitraahmadi129@gmail.com>
2024-07-26 02:55:41 +1000

    setting up error image icon to test for page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,6 +2,7 @@ export default function Error() {
     return (
         <div>
             <h1>Oopsie! Something went wrong</h1>
+            <img src="../../error.jpeg"></img>
         </div>
     );
 }
