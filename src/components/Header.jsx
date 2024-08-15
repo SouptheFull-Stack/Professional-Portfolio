b0e3e83ff583d915591745ec92c3e76db7960e5b
Mitra USYD <mitraahmadi129@gmail.com>
2024-08-15 12:53:07 +1000

    debugging css issues and importing

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import NavTabs from './Navigation';
+
+export default function Header() {
+    return (
+        <nav className='is-flex is-justify-content-center'>
+            <NavTabs /> 
+        </nav>
+    );
+}
+
+
